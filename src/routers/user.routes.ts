37e0b2519cb4246794dad2f9b31ee638fa312a2c
irfanhathoti <irfanhathoti@gmail.com>
2026-09-10 20:33:04 +0530

    feat: restrict access to getUsers endpoint for SUPER_ADMIN role

--- src/routers/user.routes.ts
+++ src/routers/user.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import UserController from "../controller/user.controller";
 import { requireAuth } from "../middleware/auth.middleware";
+import { allowRoles } from "../middleware/role.middleware";
+import { UserRole } from "../modals/user.model";
 
 const userControler = new UserController();
 
 const router = Router();
 
-router.get("/", requireAuth, userControler.getUsers);
+router.get(
+  "/",
+  requireAuth,
+  allowRoles(UserRole.SUPER_ADMIN),
+  userControler.getUsers,
+);
 router.get("/:id", requireAuth, userControler.getUser);
 
 export default router;
